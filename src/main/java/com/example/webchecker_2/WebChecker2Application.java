1e88c1811876ea0acd8026f670a645898fe4335c
Ala <Ala.bensalah5@gmail.com>
2026-06-08 23:15:42 +0200

    Initial commit

---
+++ src/main/java/com/example/webchecker_2/WebChecker2Application.java
@@ -0,0 +1,13 @@
+package com.example.webchecker_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChecker2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebChecker2Application.class, args);
+    }
+
+}
